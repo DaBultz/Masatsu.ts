0b9cb6146c09fb2776a46ecaf381126ef215b1f0
DaBultz <OfficialBultz@gmail.com>
2021-10-07 21:03:17 +0200

    weird delete

--- test/commands/ping.d.ts
+++
@@ -1,6 +0,0 @@
-import { BaseCommandInteraction } from 'discord.js';
-import { Command } from '../../src/base/command';
-export default class Ping extends Command {
-    constructor();
-    onCommand(interaction: BaseCommandInteraction): Promise<void>;
-}
--- test/events/ready.d.ts
+++
@@ -1,5 +0,0 @@
-import { Event } from '../../src/base/event';
-export default class Ready extends Event {
-    constructor();
-    onEvent(): void;
-}
