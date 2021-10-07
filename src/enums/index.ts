d7090dcfdbdc6dba9055264f0be668709f29aade
DaBultz <OfficialBultz@gmail.com>
2021-10-07 21:02:37 +0200

    Add `DiscordEvent` to export

---
+++ src/enums/index.ts
@@ -0,0 +1,5 @@
+import { DiscordEvent } from './discordEvent'
+
+export {
+  DiscordEvent
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Bot } from './bot'
 import { Command, Event } from './base'
+import { DiscordEvent } from './enums'
 
 export {
   Bot,
   Command,
-  Event
+  Event,
+  DiscordEvent
 }
